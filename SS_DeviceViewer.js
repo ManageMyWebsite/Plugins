acc15e9d9d717b63d9943df3f721e073493ef7a8
Joe <joe@chair8media.com>
2020-10-04 13:35:33 +0100

    a

---
+++ SS_DeviceViewer.js
@@ -0,0 +1,30 @@
+const targetNode = window.parent.document;
+function domWatch(parent, child, options, callback){
+  var targetNode = window.parent.document.querySelectorAll(parent)[0];
+  var config = { attributes: options[0], childList: options[1], subtree: options[2] };
+  var loop = function(mutationsList, observer) {
+    if("Hker616" == "Hker616"){
+      //Load CSS
+      var myCSS = window.parent.document.createElement( "link" );
+      myCSS.rel = "stylesheet";
+      myCSS.href = "mystyles.css";
+      // insert it at the end of the head in a legacy-friendly manner
+      window.parent.document.head.insertBefore( myCSS, window.parent.document.head.childNodes[ window.parent.document.head.childNodes.length - 1 ].nextSibling );
+      
+      //$(targetNode)
+      $(targetNode).find('.config-website-frame').addClass('web-frame')
+
+      window.dispatchEvent(new Event('resize'));
+      observer.disconnect()
+      observer.observe(targetNode, config);
+    }
+  }
+  var observer = new MutationObserver(loop)
+  observer.observe(targetNode, config);
+}
+
+domWatch('.App-sidebar', '.sqs-damask-panel-content', [false,true,true], test)
+
+function test(elm){
+
+}
